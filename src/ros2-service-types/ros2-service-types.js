de7ce3094cb9321ca53b52a92dc9e5172d9fedf9
Christian Wendt <christian.wendt@eduart-robotik.com>
2023-09-13 17:38:35 +0200

    Get all messages and services by ros cli.

--- src/ros2-service-types/ros2-service-types.js
+++ src/ros2-service-types/ros2-service-types.js
@@ -1,8 +1,8 @@
 // RED argument provides the module access to Node-RED runtime api
 module.exports = function(RED)
 {
+    var execFile = require('child_process').execFile;    
     var fs = require('fs');
-    var path = require('path');
     var ros2_home = '/opt/ros/' + process.env.ROS_DISTRO;
     if (process.env.IS_ROS2_PATH) {
         ros2_home = process.env.IS_ROS2_PATH;
@@ -43,7 +43,7 @@ module.exports = function(RED)
     RED.nodes.registerType("ROS2 Service Type", ROS2ServiceTypes);
 
     // Function that pass the IS ROS 2 compiled packages to the html file
-    RED.httpAdmin.get("/ros2packages", RED.auth.needsPermission('ROS2 Service Type.read'), function(req,res)
+    RED.httpAdmin.get("/ros2srvspackages", RED.auth.needsPermission('ROS2 Service Type.read'), function(req,res)
     {
         console.log("Estimate all packages that provide services.");
         execFile("ros2", ["interface", "list"], function(error, stdout, stderr) {    
@@ -51,7 +51,7 @@ module.exports = function(RED)
             var package_list = [];
             
             stdout.split('\n').forEach(line => {
-                if (found_service_start == false && line.include("Services:")) {
+                if (found_service_start == false && line.includes("Services:")) {
                     // found start of services --> processing list at next iteration
                     found_service_start = true;
                     return;
@@ -60,56 +60,62 @@ module.exports = function(RED)
                     // no services --> no processing
                     return;
                 }
-                if (found_service_start == true && line.include("Actions:")) {
+                if (found_service_start == true && line.includes("Actions:")) {
                     // end of services reached --> stop processing
                     found_service_start = false;
                     return;
                 }
 
-                service_list.push(line.split('/')[0]);
+                line = line.trim();
+                let package = line.split('/')[0]
+
+                if (package_list.includes(package) == false) {
+                    package_list.push(package);
+                }
             });
 
             console.log("Found following packages that provide services:");
             console.log(package_list);
             res.json(package_list);
         });
-
-        // var files = fs.readdirSync(ros2_home + "/share/");
-
-        // // Check if it is a srv package
-        // files.forEach( function(value)
-        // {
-        //     if (!fs.existsSync(ros2_home + "/share/" + value + "/srv"))
-        //     {
-        //         files = files.filter(f => f != value);
-        //     }
-        // });
-
-        // res.json(files);
     });
 
     // Function that pass the IS ROS 2 package compiled msgs to the html file
     RED.httpAdmin.get("/ros2srvs", RED.auth.needsPermission('ROS2 Service Type.read'), function(req,res)
     {
-        var msgs_path = ros2_home + "/share/" + req.query["package"] + "/srv/";
+        console.log("Estimate all services that is provided by package '" + req.query["package"] + "':");
+        execFile("ros2", ["interface", "list"], function(error, stdout, stderr) {    
+            var found_service_start = false;
+            var service_list = [];
+            
+            stdout.split('\n').forEach(line => {
+                if (found_service_start == false && line.includes("Services:")) {
+                    // found start of services --> processing list at next iteration
+                    found_service_start = true;
+                    return;
+                }
+                if (found_service_start == false) {
+                    // no services --> no processing
+                    return;
+                }
+                if (found_service_start == true && line.includes("Actions:")) {
+                    // end of services reached --> stop processing
+                    found_service_start = false;
+                    return;
+                }
 
-        var files = fs.readdirSync(msgs_path);
+                line = line.trim();
+                line_parts = line.split('/');
 
-        // Check that it is a file with .idl
-        files.forEach( function(filename)
-        {
-            if (path.extname(filename) != ".idl")
-            {
-                files = files.filter(f => f != filename);
-            }
+                if (line_parts[0] == req.query['package']) {
+                    service_list.push(line_parts[2]);
+                }
+            });
+
+            console.log("Found following services that is provided by package '" + req.query["package"] + "':");
+            console.log(service_list);
+            res.json(service_list);
         });
-
-        files.forEach( function(filename, index)
-        {
-            files[index] = path.parse(filename).name;
-        })
-
-        res.json(files);
     });
 
     // Function that pass the selected message idl and srv codes
--- src/ros2-types/ros2-types.js
+++ src/ros2-types/ros2-types.js
@@ -1,8 +1,8 @@
 // RED argument provides the module access to Node-RED runtime api
 module.exports = function(RED)
 {
+    var execFile = require('child_process').execFile;    
     var fs = require('fs');
-    var path = require('path');
     var ros2_home = '/opt/ros/' + process.env.ROS_DISTRO;
     if (process.env.IS_ROS2_PATH) {
         ros2_home = process.env.IS_ROS2_PATH;
@@ -43,44 +43,79 @@ module.exports = function(RED)
     RED.nodes.registerType("ROS2 Type", ROS2Types);
 
     // Function that pass the IS ROS 2 compiled packages to the html file
-    RED.httpAdmin.get("/ros2packages", RED.auth.needsPermission('ROS2 Type.read'), function(req,res)
+    RED.httpAdmin.get("/ros2msgspackages", RED.auth.needsPermission('ROS2 Type.read'), function(req,res)
     {
-        var files = fs.readdirSync(ros2_home + "/share/");
+        console.log("Estimate all packages that provide messages.");
+        execFile("ros2", ["interface", "list"], function(error, stdout, stderr) {    
+            var found_message_start = false;
+            var package_list = [];
+            
+            stdout.split('\n').forEach(line => {
+                if (found_message_start == false && line.includes("Messages:")) {
+                    // found start of services --> processing list at next iteration
+                    found_message_start = true;
+                    return;
+                }
+                if (found_message_start == false) {
+                    // no services --> no processing
+                    return;
+                }
+                if (found_message_start == true && line.includes("Services:")) {
+                    // end of services reached --> stop processing
+                    found_message_start = false;
+                    return;
+                }
 
-        // Check if it is a msg package
-        files.forEach( function(value)
-        {
-            if (!fs.existsSync(ros2_home + "/share/" + value + "/msg"))
-            {
-                files = files.filter(f => f != value);
-            }
+                line = line.trim();
+                let package = line.split('/')[0]
+
+                if (package_list.includes(package) == false) {
+                    package_list.push(package);
+                }
+            });
+
+            console.log("Found following packages that provide services:");
+            console.log(package_list);
+            res.json(package_list);
         });
-
-        res.json(files);
     });
 
     // Function that pass the IS ROS 2 package compiled msgs to the html file
     RED.httpAdmin.get("/ros2msgs", RED.auth.needsPermission('ROS2 Type.read'), function(req,res)
     {
-        var msgs_path = ros2_home + "/share/" + req.query["package"] + "/msg/";
+        console.log("Estimate all messages that is provided by package '" + req.query["package"] + "':");
+        execFile("ros2", ["interface", "list"], function(error, stdout, stderr) {    
+            var found_service_start = false;
+            var message_list = [];
+            
+            stdout.split('\n').forEach(line => {
+                if (found_service_start == false && line.includes("Messages:")) {
+                    // found start of services --> processing list at next iteration
+                    found_service_start = true;
+                    return;
+                }
+                if (found_service_start == false) {
+                    // no services --> no processing
+                    return;
+                }
+                if (found_service_start == true && line.includes("Services:")) {
+                    // end of services reached --> stop processing
+                    found_service_start = false;
+                    return;
+                }
 
-        var files = fs.readdirSync(msgs_path);
+                line = line.trim();
+                line_parts = line.split('/');
 
-        // Check that it is a file with .idl
-        files.forEach( function(filename)
-        {
-            if (path.extname(filename) != ".idl")
-            {
-                files = files.filter(f => f != filename);
-            }
+                if (line_parts[0] == req.query['package']) {
+                    message_list.push(line_parts[2]);
+                }
+            });
+
+            console.log("Found following services that is provided by package '" + req.query["package"] + "':");
+            console.log(message_list);
+            res.json(message_list);
         });
-
-        files.forEach( function(filename, index)
-        {
-            files[index] = path.parse(filename).name;
-        })
-
-        res.json(files);
     });
 
     // Function that pass the selected message idl and msg codes
