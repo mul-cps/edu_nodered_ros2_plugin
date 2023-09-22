8299e0e26eb83dfa1692b53f5d2fd896cbe108d9
Christian Wendt <christian.wendt@eduart-robotik.com>
2023-09-22 10:49:49 +0200

    Implement nodes for action handling.

---
+++ src/action-client/action-client.js
@@ -0,0 +1,95 @@
+const ros_node = require('../ros2/ros2-instance');
+const rclnodejs = require("rclnodejs");
+
+// RED argument provides the module access to Node-RED runtime api
+module.exports = function(RED)
+{
+    var fs = require('fs');
+    /*
+     * @function SubscriberNode constructor
+     * This node is defined by the constructor function SubscriberNode,
+     * which is called when a new instance of the node is created
+     *
+     * @param {Object} config - Contains the properties set in the flow editor
+     */
+    function ActionClientNode(config)
+    {
+        // Initialize the features shared by all nodes
+        RED.nodes.createNode(this, config);
+        this.props = config.props;
+        var node = this;
+        node.ready = false;
+
+        // \todo handle domain id differently
+        if(config.domain) {
+            // modify the global domain
+            node.domain = RED.nodes.getNode(config.domain).domain;
+        }
+
+        try {
+            console.log("creating service client...");
+            console.log("type:")
+            console.log(config['selectedtype']);
+
+            this.client = ros_node.node.createClient(config['selectedtype'], config['topic'])
+            node.ready = true;
+            node.status({ fill: "yellow", shape: "dot", text: "created"});
+            console.log("service client was created successfully");
+        }
+        catch (error) {
+            console.log("creating subscription failed");
+            console.log(error);
+            node.ready = false;
+            node.status({ fill: "red", shape: "dot", text: "error"});
+        }
+
+        // Event emitted when the deploy is finished
+        RED.events.once('flows:started', function() {
+            if (node.ready) {
+                node.status({ fill: "green", shape: "dot", text: "waiting to request service"});
+            }
+        });
+
+        // Registers a listener to the input event,
+        // which will be called whenever a message arrives at this node
+        node.on('input', function(msg) {
+            if (node.ready) {
+                if (this.client.isServiceServerAvailable() == false) {
+                    node.status({ fill: "yellow", shape: "dot", text: "service not available"});
+                    return;
+                }
+
+                // service is available and ready
+                node.status({ fill: "green", shape: "dot", text: "request published"});
+
+                this.client.sendRequest(msg.payload, function(response) {
+                    // Passes the message to the next node in the flow
+                    node.status({ fill: "green", shape: "dot", text: "response received"});
+                    node.send({ payload: response });
+                });
+            }
+            else {
+               done("node was not ready to process flow data");
+            }
+        });
+
+        // Called when there is a re-deploy or the program is closed
+        node.on('close', function() {
+            ros_node.node.destroyClient(this.client);
+            this.client = null;
+            node.status({ fill: null, shape: null, text: ""});
+        });
+    }
+
+    // The node is registered in the runtime using the name "Action Client"
+    RED.nodes.registerType("Action Client", ActionClientNode);
+
+    //Function that sends to the html file the qos descriptions read from the json file
+    RED.httpAdmin.get("/subqosdescription", RED.auth.needsPermission('Action Client.read'), function(req,res)
+    {
+        var description_path = __dirname + "/../qos-description.json";
+        var rawdata  = fs.readFileSync(description_path);
+        let json = JSON.parse(rawdata);
+        res.json(json);
+    });
+}
---
+++ src/ros2-action-types/ros2-action-types.js
@@ -0,0 +1,123 @@
+// RED argument provides the module access to Node-RED runtime api
+module.exports = function(RED)
+{
+    var execFile = require('child_process').execFile;    
+    var package_list = [];
+    var interface_list = [];
+
+    /*
+     * @function ROS2ActionTypes constructor
+     * This node is defined by the constructor function ROS2ActionTypes,
+     * which is called when a new instance of the node is created
+     *
+     * @param {Object} config - Contains the properties set in the flow editor
+     */
+    function ROS2ActionTypes(config)
+    {
+        // Initiliaze the features shared by all nodes
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        // Event emitted when the deploy is finished
+        RED.events.once("flows:started", function() {
+            node.status({ fill: "green", shape: "dot", text: ""});
+        });
+
+	    // Registers a listener to the input event,
+        // which will be called whenever a message arrives at this node
+        node.on('input', function(msg) {
+            // Passes the message to the next node in the flow
+            node.send(msg);
+        });
+
+        // Called when there is a re-deploy or the program is closed
+        node.on('close', function() {
+            node.status({ fill: null, shape: null, text: ""});            
+        });
+    }
+
+    // The node is registered in the runtime using the name publisher
+    RED.nodes.registerType("ROS2 Action Type", ROS2ActionTypes);
+
+    // Function that pass the IS ROS 2 compiled packages to the html file
+    RED.httpAdmin.get("/ros2actionpackages", RED.auth.needsPermission('ROS2 Action Type.read'), function(req,res)
+    {
+        if (package_list.length > 0) {
+            // Uses previous estimated message package list.
+            console.log("uses previous estimated action package list:");
+            console.log(package_list);
+            res.json(package_list);
+            return;
+        }
+
+        console.log("Estimate all packages that provide services.");
+        execFile("ros2", ["interface", "list"], function(error, stdout, stderr) {    
+            var found_service_start = false;
+            
+            stdout.split('\n').forEach(line => {
+                if (found_service_start == false && line.includes("Actions:")) {
+                    // found start of services --> processing list at next iteration
+                    found_service_start = true;
+                    return;
+                }
+                if (found_service_start == false) {
+                    // no services --> no processing
+                    return;
+                }
+
+                line = line.trim();
+                let package = line.split('/')[0]
+
+                if (package_list.includes(package) == false) {
+                    package_list.push(package);
+                }
+            });
+
+            console.log("Found following packages that provide services:");
+            console.log(package_list);
+            res.json(package_list);
+        });
+    });
+
+    // Function that pass the IS ROS 2 package compiled msgs to the html file
+    RED.httpAdmin.get("/ros2actions", RED.auth.needsPermission('ROS2 Action Type.read'), function(req,res)
+    {
+        if (interface_list[req.query["package"]] != undefined) {
+            // Uses previous estimated message list.
+            console.log("uses already estimated action list:");
+            console.log(interface_list[req.query["package"]]);
+            res.json(interface_list[req.query["package"]]);
+            return;
+        }
+
+        console.log("Estimate all actions that is provided by package '" + req.query["package"] + "':");
+        execFile("ros2", ["interface", "list"], function(error, stdout, stderr) {    
+            var found_service_start = false;
+            var action_list = [];
+            
+            stdout.split('\n').forEach(line => {
+                if (found_service_start == false && line.includes("Actions:")) {
+                    // found start of services --> processing list at next iteration
+                    found_service_start = true;
+                    return;
+                }
+                if (found_service_start == false) {
+                    // no services --> no processing
+                    return;
+                }
+
+                line = line.trim();
+                line_parts = line.split('/');
+
+                if (line_parts[0] == req.query['package']) {
+                    action_list.push(line_parts[2]);
+                }
+            });
+
+            console.log("Found following actions that is provided by package '" + req.query["package"] + "':");
+            console.log(action_list);
+            interface_list[req.query["package"]] = action_list;            
+            res.json(action_list);
+        });
+    });
+}
