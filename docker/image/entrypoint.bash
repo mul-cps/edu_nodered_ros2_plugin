2ecda804a92eedfa2a600820639fa9636ed3cf8e
David Kiesel <david.kiesel@eudart-robotik.com>
2024-10-23 11:59:55 +0200

    Switch from ROS2 humble to jazzy

--- docker/image/entrypoint.bash
+++ docker/image/entrypoint.bash
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Setup ROS 2  environment
-source /opt/ros/humble/setup.bash
+source /opt/ros/jazzy/setup.bash
 source /home/user/ros/install/setup.bash
 export NODE_PATH=/usr/lib/node_modules
-export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 # export NVM_DIR="$HOME/.nvm"
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 exec "$@"
