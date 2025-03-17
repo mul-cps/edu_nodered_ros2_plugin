799388cf646f030935f3c364516bd51ba024f877
Your Name <you@example.com>
2025-03-17 10:52:18 +0100

    fix ci workflow

--- docker/image/entrypoint.bash
+++ docker/image/entrypoint.bash
@@ -3,7 +3,7 @@
 # Setup ROS 2  environment
 # source /opt/ros/jazzy/setup.bash
 source /opt/ros/$ROS_DISTRO/setup.bash
-source /home/user/ros/install/setup.bash
+#source /home/user/ros/install/setup.bash
 export NODE_PATH=/usr/lib/node_modules
 # export NVM_DIR="$HOME/.nvm"
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
